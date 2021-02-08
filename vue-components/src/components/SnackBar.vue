8f2687ad6e6df5e4ecb4614da69e014633d30d98
Doug Rogers <douglasrogers@tiag.net>
2021-02-08 11:41:19 -0500

    Add additional (temporar) component to started page

---
+++ vue-components/src/components/SnackBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="SnackBar">
+      <h1>{{snackType}}</h1>
+      <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SnackBar',
+  props: {
+    snackType: String,
+    msg: String
+  }
+}
+</script>
