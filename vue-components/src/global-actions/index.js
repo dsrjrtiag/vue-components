07f88bd0ef55e68aca9a88c1f92d28058a0761a2
Doug Rogers <douglasrogers@tiag.net>
2021-02-12 09:16:15 -0500

    delete duplicate files

--- vue-components/src/global-actions/index.js
+++
@@ -1,9 +0,0 @@
-import EventBus, { ACTIONS } from '../event-bus/index';
-
-export const eventBusSnackBar = (message, color) => {
-  EventBus.$emit(ACTIONS.SNACKBAR, message, color);
-};
-
-export const eventBusKendoSnackBar = (message, type) => {
-  EventBus.$emit(ACTIONS.KENDOSNACKBAR, message, type);
-}
